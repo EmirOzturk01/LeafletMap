4ef62bbb5d43b144905cc52aed8b247956792b34
Emir Öztürk <ozturkemir2001@gmail.com>
2024-04-30 03:43:06 +0300

    HTML

--- LeafletMap/SammTeknolojiMapEmir/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ LeafletMap/SammTeknolojiMapEmir/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b67d6b36587e75e3ed5a059847499d029c5264fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
